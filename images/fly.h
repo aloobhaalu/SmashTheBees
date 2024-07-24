dac8219b4b2c436162eedccf7e3376eb3eb8ee1b
aloobhaalu <ananyabhargava12@gmail.com>
2024-07-24 12:49:31 -0400

    moving all the files of the game to git

---
+++ images/fly.h
@@ -0,0 +1,23 @@
+/*
+ * Exported with nin10kit v1.8
+ * Invocation command was nin10kit --mode=3 --resize=16x16 fly fly.png 
+ * Time-stamp: Tuesday 07/16/2024, 17:31:56
+ * 
+ * Image Information
+ * -----------------
+ * fly.png 16@16
+ * 
+ * All bug reports / feature requests are to be filed here https://github.com/TricksterGuy/nin10kit/issues
+ */
+
+#ifndef FLY_H
+#define FLY_H
+
+extern const unsigned short fly[256];
+#define FLY_SIZE 512
+#define FLY_LENGTH 256
+#define FLY_WIDTH 16
+#define FLY_HEIGHT 16
+
+#endif
+
